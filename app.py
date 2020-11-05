215dad89e509e77fb5e13130630739909df94697
rtrive <riccardo.trivellato@gmail.com>
2020-11-05 20:06:23 +0100

    Add environemnt

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
+from aws_cdk.core import Environment
 
 from infrastructure_stack import InfrastructureStack
 
 
 app = core.App()
-InfrastructureStack(app, "infrastructure")
+InfrastructureStack(app, "infrastructure", env=Environment(account="457469494885", region="eu-west-1"))
 
 app.synth()
