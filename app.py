4c2513b91ad433ea32ed905f8365c6d495ac7e80
rtrive <riccardo.trivellato@gmail.com>
2020-11-17 21:45:34 +0100

    Import organization stack

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from aws_cdk import core
 from aws_cdk.core import Environment
 
 from infrastructure_stack import InfrastructureStack
-
+from organization_stack import OrganizationeStack
 
 app = core.App()
-InfrastructureStack(app, "infrastructure", env=Environment(account="457469494885", region="eu-west-1"))
+infra = InfrastructureStack(app, "infrastructure", env=Environment(account="457469494885", region="eu-west-1"))
+OrganizationeStack(app, "organization", infra.api_gateway(), infra.rds(), env=Environment(account="457469494885", region="eu-west-1"))
 
 app.synth()
