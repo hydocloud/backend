fd5982a1d820c55d190b69db8e3a1588bc3167f0
rtrive <riccardo.trivellato@gmail.com>
2020-11-18 21:11:51 +0100

    Create rds stack

---
+++ rds_stack.py
@@ -0,0 +1,36 @@
+from aws_cdk import (
+    core,
+    aws_rds as rds,
+    aws_ec2 as ec2,
+)
+
+class RdsStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Databases
+        vpc = ec2.Vpc.from_lookup(self, "VPC", is_default=True)
+
+        self.postgres_db = rds.DatabaseInstance(
+            self,
+            "RDS",
+            database_name="db1",
+            engine=rds.DatabaseInstanceEngine.postgres(
+                version=rds.PostgresEngineVersion.VER_12_4
+            ),
+            vpc=vpc,
+            port=5432,
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE3,
+                ec2.InstanceSize.MICRO,
+            ),
+            removal_policy=core.RemovalPolicy.DESTROY,
+            deletion_protection=False,
+            credentials=rds.Credentials.from_username("loginService"),
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+        )
+
+        self.postgres_db.connections.allow_from_any_ipv4(ec2.Port.tcp(5432))
+
+    def get_rds_instance(self):
+        return self.postgres_db
