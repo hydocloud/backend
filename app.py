aca3e859c7c4f8f80d53c505dd910b13cf2864b8
rtrive <riccardo.trivellato@gmail.com>
2020-11-18 21:12:33 +0100

    Refactor naming. Add rds stack

--- app.py
+++ app.py
@@ -3,11 +3,13 @@
 from aws_cdk import core
 from aws_cdk.core import Environment
 
-from infrastructure_stack import InfrastructureStack
+from login_stack import LoginStack
 from organization_stack import OrganizationeStack
+from rds_stack import RdsStack
 
 app = core.App()
-infra = InfrastructureStack(app, "infrastructure", env=Environment(account="457469494885", region="eu-west-1"))
-OrganizationeStack(app, "organization", infra.api_gateway(), infra.rds(), env=Environment(account="457469494885", region="eu-west-1"))
+database = RdsStack(app, "rds", env=Environment(account="457469494885", region="eu-west-1"))
+login_stack = LoginStack(app, "login", database.get_rds_instance(), env=Environment(account="457469494885", region="eu-west-1"))
+OrganizationeStack(app, "organization", login_stack.api_gateway(), database.get_rds_instance(), env=Environment(account="457469494885", region="eu-west-1"))
 
 app.synth()
