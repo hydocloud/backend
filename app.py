52e8d1cdf6d0949ab96e30f3b38c5f27a1a2b965
rtrive <riccardo.trivellato@gmail.com>
2020-11-19 21:28:21 +0100

    Add env to stack name

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ from aws_cdk.core import Environment
 from login_stack import LoginStack
 from organization_stack import OrganizationeStack
 from rds_stack import RdsStack
+from dns_stack import DnsStack
 
 app = core.App()
-database = RdsStack(app, "rds", env=Environment(account="457469494885", region="eu-west-1"))
-login_stack = LoginStack(app, "login", database.get_rds_instance(), env=Environment(account="457469494885", region="eu-west-1"))
-OrganizationeStack(app, "organization", login_stack.api_gateway(), database.get_rds_instance(), env=Environment(account="457469494885", region="eu-west-1"))
+environment = 'dev'
+dns_stack = DnsStack(app, '{}-dns'.format(environment),env=Environment(account='457469494885', region='eu-west-1'))
+database = RdsStack(app, '{}-rds'.format(environment), env=Environment(account='457469494885', region='eu-west-1'))
+login_stack = LoginStack(app, '{}-login'.format(environment), database.get_rds_instance(), env=Environment(account='457469494885', region='eu-west-1'))
+OrganizationeStack(app, '{}-organization'.format(environment), login_stack.api_gateway(), database.get_rds_instance(), env=Environment(account='457469494885', region='eu-west-1'))
 
 app.synth()
