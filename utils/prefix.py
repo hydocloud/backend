dbc88bbb0b03a1c25c4f2b392d2ddca06ce59821
rtrive <riccardo.trivellato@gmail.com>
2020-11-21 22:59:26 +0100

    Function to compose record name

--- utils/prefix.py
+++ utils/prefix.py
@@ -8,3 +8,9 @@ def env_specific(logical_name):
         suffix = logical_name.name
 
     return '{}-{}'.format(deploy_env(), suffix)
+
+def domain_specific(prefix, logical_name):
+    if deploy_env() != 'prod':
+        return '{}.{}.{}'.format(prefix, deploy_env(),logical_name)
+    else:
+        return '{}.{}'.format(prefix, logical_name)
