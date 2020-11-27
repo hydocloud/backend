0152b43f132daa93a37b121f92558db992dd4264
rtrive <riccardo.trivellato@gmail.com>
2020-11-27 23:43:26 +0100

    Manage get single organizations and multiple

--- microservices/organization/get_organizations/app.py
+++ microservices/organization/get_organizations/app.py
@@ -2,7 +2,7 @@ import database
 import os
 import logging
 import json
-from get import get_organization
+from get import get_organization, get_organizations
 from models.organizations import OrganizationBase
 from aws_lambda_powertools import Tracer
 
@@ -16,13 +16,17 @@ logger = logging.getLogger(__name__)
 
 @tracer.capture_lambda_handler(capture_response=False)
 def lambda_handler(event, context):
-    
-    if 'id' in event['pathParameters']:
-        owner_id = 'e12c1545-1362-4162-9c3b-ebe2e20f2e57'
+
+    owner_id = 'e12c1545-1362-4162-9c3b-ebe2e20f2e57'
+
+    if event['pathParameters'] is not None and 'id' in event['pathParameters']:
         organization_id = event['pathParameters']['id']
         response = get_organization(owner_id, organization_id)
-    # else:
-    #     response = get_organizations()
+    elif event['queryStringParameters'] is not None and 'page' in event['queryStringParameters']:
+        page_number = event['queryStringParameters']['page']
+        response = get_organizations(owner_id, page_number)
+    else:
+        response = get_organizations(owner_id)
 
     return {
         "statusCode": response.statusCode,
