1e0722cd35ee444ee3739800ffe3406615cbda5c
rtrive <riccardo.trivellato@gmail.com>
2020-11-28 10:47:03 +0100

    Apply pylint suggestions

--- stacks/certificate_stack.py
+++ stacks/certificate_stack.py
@@ -1,3 +1,5 @@
+''' Stack to deploy https certificate '''
+
 from aws_cdk import (
     core,
     aws_route53 as route53,
@@ -6,16 +8,22 @@ from aws_cdk import (
 
 
 class CertificateStack(core.Stack):
+    '''
+    Certificate stack with init method to get data from actual route zone
+    and method to issue new certificate and validate
+    '''
+
     def __init__(
-        self, scope: core.Construct, id: str, route53: route53, **kwargs
+        self, scope: core.Construct, id: str, dns_stack: route53, **kwargs
     ) -> None:
         super().__init__(scope, id, **kwargs)
-        self.hosted_zone = route53.get_hosted_zone()
-        self.domain_name = route53.get_domain_name()
+        self.hosted_zone = dns_stack.get_hosted_zone()
+        self.domain_name = dns_stack.get_domain_name()
 
     def issue_certificate(
         self, name: str, record_name: str
     ) -> certificate_manager.Certificate:
+        ''' Issue and validate new https certficate '''
         return certificate_manager.Certificate(
             self,
             name,
