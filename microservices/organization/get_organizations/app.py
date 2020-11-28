d248e3651826fa6ea81826a3a89ae66976ce89da
rtrive <riccardo.trivellato@gmail.com>
2020-11-28 16:04:55 +0100

    Restore context variable. Fix if conditions

--- microservices/organization/get_organizations/app.py
+++ microservices/organization/get_organizations/app.py
@@ -1,4 +1,4 @@
-''' Entry point lambda'''
+""" Entry point lambda"""
 
 import logging
 from aws_lambda_powertools import Tracer
@@ -14,23 +14,38 @@ logger = logging.getLogger(__name__)
 
 
 @tracer.capture_lambda_handler(capture_response=False)
-def lambda_handler(event):
-    '''
+def lambda_handler(event, context):
+    """
     Based on both path parameters or query parameters chose if
     user require info about one organization or multiple
-    '''
+    """
 
     owner_id = "e12c1545-1362-4162-9c3b-ebe2e20f2e57"
 
-    if event["pathParameters"] is not None and "id" in event["pathParameters"]:
+    if (
+        "pathParameters" in event
+        and event["pathParameters"] is not None
+        and "id" in event["pathParameters"]
+    ):
         organization_id = event["pathParameters"]["id"]
         response = get_organization(owner_id, organization_id)
     elif (
-        event["queryStringParameters"] is not None
-        and "page" in event["queryStringParameters"]
+        "queryStringParameters" in event and event["queryStringParameters"] is not None
     ):
-        page_number = event["queryStringParameters"]["page"]
-        response = get_organizations(owner_id, page_number)
+
+        if (
+            "pageSize" in event["queryStringParameters"]
+            and "page" in event["queryStringParameters"]
+        ):
+            page_number = event["queryStringParameters"]["page"]
+            page_size = event["queryStringParameters"]["pageSize"]
+            response = get_organizations(owner_id, int(page_number), int(page_size))
+        elif "pageSize" in event["queryStringParameters"]:
+            page_size = event["queryStringParameters"]["pageSize"]
+            response = get_organizations(owner_id, page_size=int(page_size))
+        elif "page" in event["queryStringParameters"]:
+            page_number = event["queryStringParameters"]["page"]
+            response = get_organizations(owner_id, page_number=int(page_number))
     else:
         response = get_organizations(owner_id)
 
