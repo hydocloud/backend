25d3980389ea944703101d2e12237ffa73cf3d9e
rtrive <riccardo.trivellato@gmail.com>
2020-11-28 10:49:42 +0100

    Add comments. Remove unused apigw2

--- stacks/dns_stack.py
+++ stacks/dns_stack.py
@@ -1,13 +1,15 @@
+''' Stack to manage dns configuration '''
+
 from aws_cdk import (
     core,
     aws_route53 as route53,
     aws_route53_targets as targets,
     aws_apigateway as apigw,
-    aws_apigatewayv2 as apigw2,
 )
 
 
 class DnsStack(core.Stack):
+    ''' Class that implement dns management '''
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
         self.domain_name = "hydo.cloud"
@@ -16,6 +18,7 @@ class DnsStack(core.Stack):
         )
 
     def add_api_gateway_record(self, name: str, api: apigw):
+        ''' Add A record for api gw custom domain name '''
         route53.ARecord(
             self,
             name,
@@ -25,6 +28,7 @@ class DnsStack(core.Stack):
         )
 
     def add_api_gateway_v2_record(self, name: str, api):
+        ''' Add A record for http api gw custom domain name '''
         route53.ARecord(
             self,
             name,
@@ -34,7 +38,9 @@ class DnsStack(core.Stack):
         )
 
     def get_domain_name(self) -> str:
+        ''' Return domain name '''
         return self.domain_name
 
     def get_hosted_zone(self) -> route53.HostedZone:
+        ''' Return hosted zone id '''
         return self.hydo_cloud_zone
