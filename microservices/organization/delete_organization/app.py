b8342d119c30b64db582bb46fbad3dbc41964928
rtrive <riccardo.trivellato@gmail.com>
2020-12-02 21:33:20 +0100

    Lint and refactor connection

--- microservices/organization/delete_organization/app.py
+++ microservices/organization/delete_organization/app.py
@@ -4,6 +4,7 @@ import logging
 import json
 from delete import delete_organization
 from models.organizations import OrganizationBase
+from database import init_db
 from aws_lambda_powertools import Tracer
 
 tracer = Tracer(service="delete_organization")
@@ -14,12 +15,20 @@ logging.basicConfig(level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
 
+CONNECTION = None
+
 
 @tracer.capture_lambda_handler(capture_response=False)
 def lambda_handler(event, context):
+    global CONNECTION
+
+    if CONNECTION == None:
+        CONNECTION = init_db()
 
     owner_id = event["requestContext"]["authorizer"]["lambda"]["sub"]
     organization_id = event["pathParameters"]["id"]
-    response = delete_organization(owner_id, organization_id)
+    response = delete_organization(
+        owner_id=owner_id, organization_id=organization_id, connection=CONNECTION
+    )
 
     return {"statusCode": response.statusCode, "body": response.body.json()}
--- microservices/organization/delete_organization/delete.py
+++ microservices/organization/delete_organization/delete.py
@@ -1,6 +1,12 @@
 from models.organizations import Organization, OrganizationsList, ResponseModel
-from models.api_response import LambdaErrorResponse, LambdaSuccessResponseWithoutData, Message, Data
+from models.api_response import (
+    LambdaErrorResponse,
+    LambdaSuccessResponseWithoutData,
+    Message,
+    Data,
+)
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm.session import Session
 import logging
 from database import init_db
 import datetime
@@ -10,42 +16,29 @@ tracer = Tracer(service="delete_organization")
 
 logger = logging.getLogger(__name__)
 
-conn = None
 
 @tracer.capture_method
-def delete_organization(owner_id, organization_id):
-    global conn
+def delete_organization(owner_id: str, organization_id: str, connection: Session):
 
-    if conn == None:
-        conn = init_db()
-        
     try:
-        res = conn.query(Organization).filter_by(
-            owner_id=owner_id,
-            id=organization_id
-        ).delete()
+        res = (
+            connection.query(Organization)
+            .filter_by(owner_id=owner_id, id=organization_id)
+            .delete()
+        )
         logger.info(res)
-        conn.commit()
+        connection.commit()
         if res == 0:
             return LambdaSuccessResponseWithoutData(
-                statusCode=404, 
-                body=(
-                    Message(message="Not found")
-                )
+                statusCode=404, body=(Message(message="Not found"))
             )
         # Call user group
         return LambdaSuccessResponseWithoutData(
-            statusCode=201, 
-            body=(
-                Message(message="ok")
-            )
+            statusCode=201, body=(Message(message="ok"))
         )
     except SQLAlchemyError as e:
-        logger.error(e)  
-        conn.rollback()   
+        logger.error(e)
+        connection.rollback()
         return LambdaErrorResponse(
-            body=(
-                Message(message="Internal Server Error")
-            ), 
-            statusCode=500
+            body=(Message(message="Internal Server Error")), statusCode=500
         )
