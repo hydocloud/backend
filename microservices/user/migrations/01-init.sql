673a654f44f92ecf54f2f8183ba94d267a374a91
rtrive <riccardo.trivellato@gmail.com>
2020-12-06 13:12:25 +0100

    Change init migration

--- microservices/user/migrations/01-init.sql
+++ microservices/user/migrations/01-init.sql
@@ -1,12 +1,18 @@
 CREATE TABLE "user_groups" (
   "id" SERIAL PRIMARY KEY,
   "name" varchar,
-  "organization_id" int
+  "organization_id" int,
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "user_belong_user_groups" (
   "id" SERIAL PRIMARY KEY,
   "user_id" uuid,
-  "user_group_id" int,
-  "attributes" varchar
+  "user_group_id" int UNIQUE,
+  "attributes" varchar,
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp NOT NULL DEFAULT (now())
 );
+
+ALTER TABLE "user_groups" ADD FOREIGN KEY ("id") REFERENCES "user_belong_user_groups" ("user_group_id");
