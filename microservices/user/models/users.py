37aa7838064020c18e155db897ce8def285fba45
rtrive <riccardo.trivellato@gmail.com>
2020-12-07 11:04:11 +0100

    Add delete cascade on model

--- microservices/user/models/users.py
+++ microservices/user/models/users.py
@@ -14,6 +14,7 @@ class UserGroups(Base):
     name = Column(String)
     organization_id = Column(Integer)
     owner_id = Column(UUID(as_uuid=True))
+    user_group_belog_to_user = relationship("UserBelongUserGroups", cascade="all, delete")  
     created_at = Column(DateTime, nullable=False)
     updated_at = Column(DateTime, nullable=False)
 
