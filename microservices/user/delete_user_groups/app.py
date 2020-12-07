4ca256d370ec39d6a8005daa1c4590d40b07c99b
rtrive <riccardo.trivellato@gmail.com>
2020-12-07 11:04:40 +0100

    Add delete user group

---
+++ microservices/user/delete_user_groups/app.py
@@ -0,0 +1,40 @@
+import database
+import os
+import logging
+import json
+from delete import delete_user_groups
+from models.users import UserGroupsApiInput
+from database import init_db
+from aws_lambda_powertools import Tracer
+from pydantic import ValidationError
+
+
+tracer = Tracer(service="delete_user_group")
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+CONNECTION = None
+
+
+@tracer.capture_lambda_handler(capture_response=False)
+def lambda_handler(event, context):
+    global CONNECTION
+
+    if CONNECTION == None:
+        CONNECTION = init_db()
+
+    try:
+        owner_id = "ddd6de86-52be-447b-a8e2-54f40fa78cd1"  # event["requestContext"]["authorizer"]["lambda"]["sub"]
+        user_group_id = event["pathParameters"]["id"]
+        response = delete_user_groups(
+            owner_id=owner_id, user_group_id=user_group_id, connection=CONNECTION
+        )
+    except ValidationError:
+        logger.error("Validation input error")
+        return {"statusCode": 400, "body": {"message": "Bad request"}}
+
+    return {"statusCode": response.statusCode, "body": json.dumps(response.body)}
---
+++ microservices/user/delete_user_groups/delete.py
@@ -0,0 +1,55 @@
+""" Delete user group """
+
+import logging
+from models.users import UserGroups
+from models.api_response import LambdaResponse
+from pydantic import ValidationError
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm.session import Session
+from aws_lambda_powertools import Tracer
+
+tracer = Tracer(service="delete_user_group")
+
+logger = logging.getLogger(__name__)
+
+
+@tracer.capture_method
+def delete_user_groups(owner_id: str, user_group_id: int, connection: Session) -> LambdaResponse:
+    """ Function that delete user group on db """
+
+    try:
+        user_group = connection.query(UserGroups).filter_by(owner_id=owner_id, id=user_group_id).delete()
+
+        connection.commit()
+
+        if user_group == 0:
+            status_code = 404
+            message = "Not found"
+        else:
+            status_code = 201
+            message = "Ok"
+
+        return LambdaResponse(
+            statusCode=status_code,
+            body= {
+                "message": message
+            }
+        )
+    except SQLAlchemyError as e:
+        logger.error(e)
+        connection.rollback()
+        return LambdaResponse(
+            statusCode = 500,
+            body={
+                "message": "Internal server Error"
+            }
+        )
+    except ValidationError as e:
+        logger.error(e)
+        connection.rollback()
+        return LambdaResponse(
+            statusCode = 400,
+            body={
+                "message": "Bad request"
+            }
+        )
