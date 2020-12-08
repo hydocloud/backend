eeb7ddabfdb3f834bcea359acf1fa97f38eaacb4
rtrive <riccardo.trivellato@gmail.com>
2020-12-08 18:45:20 +0100

    Update migrations

--- microservices/user/migrations/01-init.sql
+++ microservices/user/migrations/01-init.sql
@@ -10,10 +10,11 @@ CREATE TABLE "user_groups" (
 CREATE TABLE "user_belong_user_groups" (
   "id" SERIAL PRIMARY KEY,
   "user_id" uuid NOT NULL,
-  "user_group_id" int UNIQUE NOT NULL,
+  "user_group_id" int NOT NULL,
   "attributes" varchar,
   "created_at" timestamp NOT NULL DEFAULT (now()),
-  "updated_at" timestamp NOT NULL DEFAULT (now())
+  "updated_at" timestamp NOT NULL DEFAULT (now()),
+  UNIQUE("user_id", "user_group_id")
 );
 
 ALTER TABLE "user_belong_user_groups" ADD FOREIGN KEY ("user_group_id") REFERENCES "user_groups" ("id") ON DELETE CASCADE;
