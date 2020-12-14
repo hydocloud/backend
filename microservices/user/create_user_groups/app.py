2bda3f1669b8cb209c501601d219e4f9cb093846
rtrive <riccardo.trivellato@gmail.com>
2020-12-14 22:39:19 +0100

    Parse records not valid to consume from sqs

--- microservices/user/create_user_groups/app.py
+++ microservices/user/create_user_groups/app.py
@@ -6,6 +6,7 @@ from models.api_response import Message, LambdaResponse
 from database import init_db
 from aws_lambda_powertools import Tracer
 from pydantic import ValidationError
+from typing import Tuple
 
 
 tracer = Tracer(service="create_user_group")
@@ -19,7 +20,7 @@ logger = logging.getLogger(__name__)
 CONNECTION = None
 
 
-def parse_input(event: dict) -> dict:
+def parse_input(event: dict) -> Tuple[dict, str]:
     """ Parse input to check if it's came from api or sqs """
     try:
         owner_id = event["requestContext"]["authorizer"]["lambda"]["sub"]
@@ -29,6 +30,7 @@ def parse_input(event: dict) -> dict:
     except (ValidationError, KeyError) as err:
         logger.info(err)
     try:
+        logger.info(type(event["Records"][0]["body"]))
         message = json.loads(event["Records"][0]["body"])
         owner_id = message["ownerId"]
         del message['ownerId']
@@ -36,11 +38,12 @@ def parse_input(event: dict) -> dict:
     except (ValidationError, KeyError) as err:
         logger.info(err)
 
-    return {}
+    return {}, None
 
 
 @tracer.capture_lambda_handler(capture_response=False)
 def lambda_handler(event, context):
+    logger.info(event)
     global CONNECTION
 
     if CONNECTION is None:
@@ -48,6 +51,8 @@ def lambda_handler(event, context):
 
     try:
         payload, owner_id = parse_input(event)
+        if len(payload) == 0 and owner_id is None:
+            return
         m = UserGroupsApiInput.parse_obj(payload)
         response = create_user_groups(owner_id, m, CONNECTION)
     except ValidationError:
