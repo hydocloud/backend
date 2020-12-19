d774b4c473ff684091d90861c8b4cb66028d599b
rtrive <riccardo.trivellato@gmail.com>
2020-12-19 14:29:56 +0100

    Device groups get lambda

---
+++ microservices/device/get_device_groups/app.py
@@ -0,0 +1,68 @@
+""" Entry point lambda"""
+
+import logging
+from aws_lambda_powertools import Tracer
+from get import get_device_groups
+from database import init_db
+
+tracer = Tracer(service="get_device_groups")
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+CONNECTION = None
+
+
+@tracer.capture_lambda_handler(capture_response=False)
+def lambda_handler(event, context):
+    """
+    Based on both path parameters or query parameters chose if
+    device require info about one device group or multiple
+    """
+    global CONNECTION
+    owner_id = event["requestContext"]["authorizer"]["lambda"]["sub"]
+
+    if CONNECTION is None:
+        CONNECTION = init_db()
+
+    if (
+        "pathParameters" in event
+        and event["pathParameters"] is not None
+        and "id" in event["pathParameters"]
+    ):
+        device_group_id = event["pathParameters"]["id"]
+        response = get_device_groups(
+            connection=CONNECTION, owner_id=owner_id, device_group_id=device_group_id
+        )
+    elif (
+        "queryStringParameters" in event and event["queryStringParameters"] is not None
+    ):
+
+        if (
+            "pageSize" in event["queryStringParameters"]
+            and "page" in event["queryStringParameters"]
+        ):
+            page_number = event["queryStringParameters"]["page"]
+            page_size = event["queryStringParameters"]["pageSize"]
+            response = get_device_groups(
+                connection=CONNECTION,
+                owner_id=owner_id,
+                page_number=int(page_number),
+                page_size=int(page_size),
+            )
+        elif "pageSize" in event["queryStringParameters"]:
+            page_size = event["queryStringParameters"]["pageSize"]
+            response = get_device_groups(
+                connection=CONNECTION, owner_id=owner_id, page_size=int(page_size)
+            )
+        elif "page" in event["queryStringParameters"]:
+            page_number = event["queryStringParameters"]["page"]
+            response = get_device_groups(
+                connection=CONNECTION, owner_id=owner_id, page_number=int(page_number)
+            )
+    else:
+        response = get_device_groups(connection=CONNECTION, owner_id=owner_id)
+
+    return response
---
+++ microservices/device/get_device_groups/get.py
@@ -0,0 +1,70 @@
+"""
+Module get device_group to have list of device_groups filter by owner_id
+You can select on single device_group or multiple device_group
+"""
+
+import logging
+from typing import List
+from models.devices import DeviceGroups, DeviceGroupsModelShort
+from models.api_response import LambdaResponse, DeviceGroupsList, DataModel, Message
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm.session import Session
+
+from aws_lambda_powertools import Tracer
+from sqlalchemy_paginator import Paginator
+from pydantic import ValidationError, parse_obj_as
+
+tracer = Tracer(service="get_device_group")
+
+logger = logging.getLogger(__name__)
+
+
+@tracer.capture_method
+def get_device_groups(
+    connection: Session,
+    owner_id: str,
+    device_group_id: str = None,
+    page_number: int = 1,
+    page_size: int = 5,
+) -> LambdaResponse:
+    """ Get data about one device group or multiple"""
+
+    try:
+        if device_group_id is None:
+            res = connection.query(DeviceGroups).filter(
+                DeviceGroups.owner_id == owner_id
+            )
+        else:
+            res = connection.query(DeviceGroups).filter(
+                DeviceGroups.owner_id == owner_id, DeviceGroups.id == device_group_id
+            )
+
+        paginator = Paginator(res, page_size)
+        page = paginator.page(page_number)
+
+        m = DeviceGroupsList(
+            deviceGroups=parse_obj_as(List[DeviceGroupsModelShort], page.object_list)
+        )
+
+        if len(m.deviceGroups) == 0:
+            status_code = 404
+            body = Message(message="Not found").json()
+        else:
+            status_code = 200
+            body = DataModel(
+                data=m,
+                total=page.paginator.count,
+                totalPages=page.paginator.total_pages,
+                nextPage=(page.next_page_number if page.has_next() else None),
+                previousPage=(
+                    page.previous_page_number if page.has_previous() else None
+                ),
+            ).json(by_alias=True)
+
+        return LambdaResponse(statusCode=status_code, body=body).dict()
+
+    except (SQLAlchemyError, ValidationError) as err:
+        logger.error(err)
+        return LambdaResponse(
+            statusCode=500, body=Message(message="Internal server error").json()
+        ).dict()
