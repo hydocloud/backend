80144bfa686fa004fe4fa6024d7f6e2eec9bdc5c
rtrive <riccardo.trivellato@gmail.com>
2020-12-19 14:22:48 +0100

    Delete device group lambda

---
+++ microservices/device/delete_device_group/app.py
@@ -0,0 +1,39 @@
+import logging
+from delete import delete_device_groups
+from models.api_response import Message, LambdaResponse
+from database import init_db
+from aws_lambda_powertools import Tracer
+from pydantic import ValidationError
+
+
+tracer = Tracer(service="delete_device_group")
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+CONNECTION = None
+
+
+@tracer.capture_lambda_handler(capture_response=False)
+def lambda_handler(event, context):
+    global CONNECTION
+
+    if CONNECTION is None:
+        CONNECTION = init_db()
+
+    try:
+        owner_id = event["requestContext"]["authorizer"]["lambda"]["sub"]
+        device_group_id = event["pathParameters"]["id"]
+        response = delete_device_groups(
+            owner_id=owner_id, device_group_id=device_group_id, connection=CONNECTION
+        )
+    except ValidationError:
+        logger.error("Validation input error")
+        return LambdaResponse(
+            statusCode=400, body=Message(message="Bad request").json()
+        ).dict()
+
+    return response
---
+++ microservices/device/delete_device_group/delete.py
@@ -0,0 +1,45 @@
+""" Delete device group """
+
+import logging
+from models.devices import DeviceGroups
+from models.api_response import LambdaResponse, Message
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm.session import Session
+from aws_lambda_powertools import Tracer
+
+tracer = Tracer(service="delete_device_group")
+
+logger = logging.getLogger(__name__)
+
+
+@tracer.capture_method
+def delete_device_groups(
+    owner_id: str, device_group_id: int, connection: Session
+) -> LambdaResponse:
+    """ Function that delete device group on db """
+
+    try:
+        device_group = (
+            connection.query(DeviceGroups)
+            .filter_by(owner_id=owner_id, id=device_group_id)
+            .delete()
+        )
+
+        connection.commit()
+
+        if device_group == 0:
+            status_code = 404
+            message = "Not found"
+        else:
+            status_code = 201
+            message = "Ok"
+
+        return LambdaResponse(
+            statusCode=status_code, body=Message(message=message).json()
+        ).dict()
+    except SQLAlchemyError as e:
+        logger.error(e)
+        connection.rollback()
+        return LambdaResponse(
+            statusCode=500, body=Message(message="Internal server error").json()
+        ).dict()
