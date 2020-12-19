dc86e5b421163d0e6ec538ca3dfc215a097f01e2
rtrive <riccardo.trivellato@gmail.com>
2020-12-19 11:31:07 +0100

    Create device group lambda

---
+++ microservices/device/create_device_group/app.py
@@ -0,0 +1,64 @@
+import logging
+import json
+from create import create_device_groups
+from models.devices import DeviceGroupsApiInput
+from models.api_response import Message, LambdaResponse
+from database import init_db
+from aws_lambda_powertools import Tracer
+from pydantic import ValidationError
+from typing import Tuple
+
+
+tracer = Tracer(service="create_device_group")
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+CONNECTION = None
+
+
+def parse_input(event: dict) -> Tuple[dict, str]:
+    """ Parse input to check if it's came from api or sqs """
+    try:
+        owner_id = event["requestContext"]["authorizer"]["lambda"]["sub"]
+        message = json.loads(event["body"])
+        DeviceGroupsApiInput.parse_obj(message)
+        return json.loads(event["body"]), owner_id
+    except (ValidationError, KeyError) as err:
+        logger.info(err)
+    try:
+        logger.info(type(event["Records"][0]["body"]))
+        message = json.loads(event["Records"][0]["body"])
+        owner_id = message["ownerId"]
+        del message['ownerId']
+        return message, owner_id
+    except (ValidationError, KeyError) as err:
+        logger.info(err)
+
+    return {}, None
+
+
+@tracer.capture_lambda_handler(capture_response=False)
+def lambda_handler(event, context):
+    logger.info(event)
+    global CONNECTION
+
+    if CONNECTION is None:
+        CONNECTION = init_db()
+
+    try:
+        payload, owner_id = parse_input(event)
+        if len(payload) == 0 and owner_id is None:
+            return
+        m = DeviceGroupsApiInput.parse_obj(payload)
+        response = create_device_groups(owner_id, m, CONNECTION)
+    except ValidationError:
+        logger.error("Validation input error")
+        return LambdaResponse(
+            statusCode=400, body=Message(message="Bad request").json()
+        ).dict
+
+    return response
---
+++ microservices/device/create_device_group/create.py
@@ -0,0 +1,59 @@
+""" Create device groups """
+
+import logging
+import datetime
+from typing import List
+from models.devices import (
+    DeviceGroups,
+    DeviceGroupsApiInput,
+    DeviceGroupsModelShort,
+)
+from models.api_response import LambdaResponse, DataModel, DeviceGroupsList, Message
+from pydantic import ValidationError, parse_obj_as
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm.session import Session
+from aws_lambda_powertools import Tracer
+
+tracer = Tracer(service="create_device_group")
+
+logger = logging.getLogger(__name__)
+
+
+@tracer.capture_method
+def create_device_groups(
+    owner_id: str, payload: DeviceGroupsApiInput, connection: Session
+) -> LambdaResponse:
+    """ Function that create device group on db """
+
+    try:
+        device_groups = DeviceGroups(
+            name=payload.name,
+            organization_id=payload.organizationId,
+            owner_id=owner_id,
+            created_at=datetime.datetime.utcnow(),
+            updated_at=datetime.datetime.utcnow(),
+        )
+        connection.add(device_groups)
+        connection.commit()
+        connection.refresh(device_groups)
+
+        body = DataModel(
+            data=DeviceGroupsList(
+                deviceGroups=parse_obj_as(List[DeviceGroupsModelShort], [device_groups])
+            )
+        ).json(exclude_none=True, by_alias=True)
+
+        return LambdaResponse(statusCode=201, body=body).dict()
+
+    except SQLAlchemyError as err:
+        logger.error(err)
+        connection.rollback()
+        return LambdaResponse(
+            statusCode=500, body=Message(message="Internal server Error").json()
+        ).dict()
+    except ValidationError as e:
+        logger.error(e)
+        connection.rollback()
+        return LambdaResponse(
+            statusCode=400, body=Message(message="Bad request").json()
+        ).dict()
