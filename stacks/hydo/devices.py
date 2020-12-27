072078ac5af8e268ff5d9dfd3c10b798dab17ef7
rtrive <riccardo.trivellato@gmail.com>
2020-12-27 22:29:30 +0100

    Move from format to f string. Add devices lambda

--- stacks/hydo/devices.py
+++ stacks/hydo/devices.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     aws_lambda as _lambda,
+    aws_lambda_python as lambda_python,
     aws_apigatewayv2_integrations as apigw2_integrations,
 )
 from aws_cdk.aws_apigatewayv2 import HttpMethod
@@ -10,13 +11,13 @@ DEVICE_FOLDER = "/device"
 
 
 def lambdas(self):
-    path = self.current_path
+    PATH = self.current_PATH
 
     create_device_group_lambda = _lambda.Function(
         self,
         "CreateDeviceGroup",
         runtime=_lambda.Runtime.PYTHON_3_8,
-        code=_lambda.Code.asset("{}/device/create_device_group".format(path)),
+        code=_lambda.Code.asset(f"{PATH}{DEVICE_FOLDER}/create_device_group"),
         handler=LAMBDA_HANDLER,
         tracing=_lambda.Tracing.ACTIVE,
         environment={
@@ -44,7 +45,7 @@ def lambdas(self):
         self,
         "DeleteDeviceGroup",
         runtime=_lambda.Runtime.PYTHON_3_8,
-        code=_lambda.Code.asset("{}/device/delete_device_group".format(path)),
+        code=_lambda.Code.asset(f"{PATH}{DEVICE_FOLDER}/delete_device_group"),
         handler=LAMBDA_HANDLER,
         tracing=_lambda.Tracing.ACTIVE,
         environment={
@@ -69,7 +70,7 @@ def lambdas(self):
         self,
         "EditDeviceGroup",
         runtime=_lambda.Runtime.PYTHON_3_8,
-        code=_lambda.Code.asset("{}/device/edit_device_group".format(path)),
+        code=_lambda.Code.asset(f"{PATH}/device/edit_device_group"),
         handler=LAMBDA_HANDLER,
         tracing=_lambda.Tracing.ACTIVE,
         environment={
@@ -82,9 +83,9 @@ def lambdas(self):
         },
         layers=[
             self.create_dependencies_layer(
-                "Depencendies", "EditDeviceGroup", "/device/edit_device_group"
+                "Depencendies", "EditDeviceGroup", f"{DEVICE_FOLDER}/edit_device_group"
             ),
-            self.create_model_layer("ModelLayer", "EditDeviceGroup", "/device"),
+            self.create_model_layer("ModelLayer", "EditDeviceGroup", DEVICE_FOLDER),
         ],
     )
 
@@ -92,7 +93,7 @@ def lambdas(self):
         self,
         "GetDeviceGroups",
         runtime=_lambda.Runtime.PYTHON_3_8,
-        code=_lambda.Code.asset("{}{}/get_device_groups".format(path, DEVICE_FOLDER)),
+        code=_lambda.Code.asset(f"{PATH}{DEVICE_FOLDER}/get_device_groups"),
         handler=LAMBDA_HANDLER,
         tracing=_lambda.Tracing.ACTIVE,
         environment={
@@ -148,5 +149,133 @@ def lambdas(self):
         methods=[HttpMethod.GET],
         integration=apigw2_integrations.LambdaProxyIntegration(
             handler=get_device_groups_lambda
+        ),
+    )
+
+    create_device_lambda = lambda_python.PythonFunction(
+        self,
+        "CreateDevice",
+        entry=f"{PATH}{DEVICE_FOLDER}/create_device",
+        index="app.py",
+        handler="lambda_handler",
+        runtime=_lambda.Runtime.PYTHON_3_8,
+        tracing=_lambda.Tracing.ACTIVE,
+        environment={
+            "DB_PORT": self.rds.db_instance_endpoint_port,
+            "DB_HOST": self.rds.db_instance_endpoint_address,
+            "DB_NAME": "devices",
+            "DB_ENGINE": "postgresql",
+            "DB_USER": "loginService",
+            "DB_PASSWORD": "ciaociao",
+        },
+        layers=[
+            self.create_model_layer("ModelLayer", "CreateDevice", DEVICE_FOLDER),
+        ],
+    )
+
+    create_device_lambda.add_event_source(
+        source=SqsEventSource(self.create_authorization_device_queue, batch_size=1)
+    )
+
+    delete_device_lambda = lambda_python.PythonFunction(
+        self,
+        "DeleteDevice",
+        runtime=_lambda.Runtime.PYTHON_3_8,
+        entry=f"{PATH}{DEVICE_FOLDER}/delete_device",
+        index="app.py",
+        handler="lambda_handler",
+        tracing=_lambda.Tracing.ACTIVE,
+        environment={
+            "DB_PORT": self.rds.db_instance_endpoint_port,
+            "DB_HOST": self.rds.db_instance_endpoint_address,
+            "DB_NAME": "devices",
+            "DB_ENGINE": "postgresql",
+            "DB_USER": "loginService",
+            "DB_PASSWORD": "ciaociao",
+        },
+        layers=[
+            self.create_model_layer("ModelLayer", "DeleteDevice", DEVICE_FOLDER),
+        ],
+    )
+
+    edit_device_lambda = lambda_python.PythonFunction(
+        self,
+        "EditDevice",
+        runtime=_lambda.Runtime.PYTHON_3_8,
+        entry=f"{PATH}{DEVICE_FOLDER}/edit_device",
+        index="app.py",
+        handler="lambda_handler",
+        tracing=_lambda.Tracing.ACTIVE,
+        environment={
+            "DB_PORT": self.rds.db_instance_endpoint_port,
+            "DB_HOST": self.rds.db_instance_endpoint_address,
+            "DB_NAME": "devices",
+            "DB_ENGINE": "postgresql",
+            "DB_USER": "loginService",
+            "DB_PASSWORD": "ciaociao",
+        },
+        layers=[
+            self.create_model_layer("ModelLayer", "EditDevice", "/device"),
+        ],
+    )
+
+    get_devices_lambda = lambda_python.PythonFunction(
+        self,
+        "GetDevices",
+        runtime=_lambda.Runtime.PYTHON_3_8,
+        entry=f"{PATH}{DEVICE_FOLDER}/get_devices",
+        index="app.py",
+        handler="lambda_handler",
+        tracing=_lambda.Tracing.ACTIVE,
+        environment={
+            "DB_PORT": self.rds.db_instance_endpoint_port,
+            "DB_HOST": self.rds.db_instance_endpoint_address,
+            "DB_NAME": "devices",
+            "DB_ENGINE": "postgresql",
+            "DB_USER": "loginService",
+            "DB_PASSWORD": "ciaociao",
+        },
+        layers=[
+            self.create_model_layer("ModelLayer", "GetDevice", DEVICE_FOLDER),
+        ],
+    )
+
+    self.http_api.add_routes(
+        path="/devices",
+        methods=[HttpMethod.POST],
+        integration=apigw2_integrations.LambdaProxyIntegration(
+            handler=create_device_lambda
+        ),
+    )
+
+    self.http_api.add_routes(
+        path="/devices/{id}",
+        methods=[HttpMethod.PUT],
+        integration=apigw2_integrations.LambdaProxyIntegration(
+            handler=edit_device_lambda
+        ),
+    )
+
+    self.http_api.add_routes(
+        path="/devices/{id}",
+        methods=[HttpMethod.DELETE],
+        integration=apigw2_integrations.LambdaProxyIntegration(
+            handler=delete_device_lambda
+        ),
+    )
+
+    self.http_api.add_routes(
+        path="/devices",
+        methods=[HttpMethod.GET],
+        integration=apigw2_integrations.LambdaProxyIntegration(
+            handler=get_devices_lambda
+        ),
+    )
+
+    self.http_api.add_routes(
+        path="/devices/{id}",
+        methods=[HttpMethod.GET],
+        integration=apigw2_integrations.LambdaProxyIntegration(
+            handler=get_devices_lambda
         ),
     )
