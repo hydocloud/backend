548d8a1fff8986c17cb5f79b6fce1f03428af7ba
rtrive <riccardo.trivellato@gmail.com>
2020-12-29 14:13:47 +0100

    Store nonce lambda

---
+++ microservices/authorization/tests/test_authorization_service.py
@@ -0,0 +1,31 @@
+from authorization_service.store_nonce import store
+
+
+def test_store_nonce_ok(dynamodb, monkeypatch):
+    monkeypatch.setenv("AUTHORIZATION_ID", "7bacf67c-4c3b-4e30-83fb-a96a93fd2c74")
+    monkeypatch.setenv("NONCE_TABLE_NAME", "authorization_sessions")
+    monkeypatch.setenv("DYNAMODB_ENDPOINT_OVERRIDE", "")
+    message = "test"
+
+    res = store(message, dynamodb)
+    assert res is True
+
+
+def test_store_nonce_ko(dynamodb, monkeypatch):
+    monkeypatch.setenv("AUTHORIZATION_ID", "7bacf67c-4c3b-4e30-83fb-a96a93fd2c74")
+    monkeypatch.setenv("NONCE_TABLE_NAME", "authorization_sessions")
+    monkeypatch.setenv("DYNAMODB_ENDPOINT_OVERRIDE", "sadsadasd")
+    message = "test"
+
+    res = store(message, dynamodb)
+    assert res is False
+
+
+def test_store_nonce_ko_with_no_dynamodb(dynamodb, monkeypatch):
+    monkeypatch.setenv("AUTHORIZATION_ID", "7bacf67c-4c3b-4e30-83fb-a96a93fd2c74")
+    monkeypatch.setenv("NONCE_TABLE_NAME", "authorization_sessions")
+    monkeypatch.setenv("DYNAMODB_ENDPOINT_OVERRIDE", "sadsadasd")
+    message = "test"
+
+    res = store(message, None)
+    assert res is False
