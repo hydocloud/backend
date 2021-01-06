cda31b6bc7d24d53a99f7f7b55e2a1d7a6d92a5c
rtrive <riccardo.trivellato@gmail.com>
2021-01-06 17:51:54 +0100

    Delete authz tests

---
+++ microservices/authorization/tests/test_delete_authorization.py
@@ -0,0 +1,118 @@
+import pytest
+from delete_authorization.delete import delete_authorization
+from delete_authorization import app
+from delete_authorization.app import lambda_handler
+
+
+@pytest.fixture
+def apigw_event():
+    """ Generate SQS event """
+    return {
+        "body": None,
+        "resource": "/{proxy+}",
+        "path": "/path/to/resource",
+        "httpMethod": "POST",
+        "isBase64Encoded": True,
+        "queryStringParameters": {"foo": "bar"},
+        "multiValueQueryStringParameters": {"foo": ["bar"]},
+        "pathParameters": {"id": 1},
+        "stageVariables": {"baz": "qux"},
+        "headers": {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, sdch",
+            "Accept-Language": "en-US,en;q=0.8",
+            "Cache-Control": "max-age=0",
+            "CloudFront-Forwarded-Proto": "https",
+            "CloudFront-Is-Desktop-Viewer": "true",
+            "CloudFront-Is-Mobile-Viewer": "false",
+            "CloudFront-Is-SmartTV-Viewer": "false",
+            "CloudFront-Is-Tablet-Viewer": "false",
+            "CloudFront-Viewer-Country": "US",
+            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
+            "Upgrade-Insecure-Requests": "1",
+            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
+            "X-Amz-Cf-Id": "cDehVQoZnx43VYQb9j2-nvCh-9z396Uhbp027Y2JvkCPNLmGJHqlaA==",
+            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
+            "X-Forwarded-Port": "443",
+            "X-Forwarded-Proto": "https",
+        },
+        "multiValueHeaders": {
+            "Accept": [
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+            ],
+            "Accept-Encoding": ["gzip, deflate, sdch"],
+            "Accept-Language": ["en-US,en;q=0.8"],
+            "Cache-Control": ["max-age=0"],
+            "CloudFront-Forwarded-Proto": ["https"],
+            "CloudFront-Is-Desktop-Viewer": ["true"],
+            "CloudFront-Is-Mobile-Viewer": ["false"],
+            "CloudFront-Is-SmartTV-Viewer": ["false"],
+            "CloudFront-Is-Tablet-Viewer": ["false"],
+            "CloudFront-Viewer-Country": ["US"],
+            "Host": ["0123456789.execute-api.us-east-1.amazonaws.com"],
+            "Upgrade-Insecure-Requests": ["1"],
+            "User-Agent": ["Custom User Agent String"],
+            "Via": ["1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)"],
+            "X-Amz-Cf-Id": ["cDehVQoZnx43VYQb9j2-nvCh-9z396Uhbp027Y2JvkCPNLmGJHqlaA=="],
+            "X-Forwarded-For": ["127.0.0.1, 127.0.0.2"],
+            "X-Forwarded-Port": ["443"],
+            "X-Forwarded-Proto": ["https"],
+        },
+        "requestContext": {
+            "authorizer": {"lambda": {"sub": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef"}},
+            "accountId": "123456789012",
+            "resourceId": "123456",
+            "stage": "prod",
+            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
+            "requestTime": "09/Apr/2015:12:34:56 +0000",
+            "requestTimeEpoch": 1428582896000,
+            "identity": {
+                "cognitoIdentityPoolId": None,
+                "accountId": None,
+                "cognitoIdentityId": None,
+                "caller": None,
+                "accessKey": None,
+                "sourceIp": "127.0.0.1",
+                "cognitoAuthenticationType": None,
+                "cognitoAuthenticationProvider": None,
+                "userArn": None,
+                "userAgent": "Custom User Agent String",
+                "user": None,
+            },
+            "path": "/prod/path/to/resource",
+            "resourcePath": "/{proxy+}",
+            "httpMethod": "POST",
+            "apiId": "1234567890",
+            "protocol": "HTTP/1.1",
+        },
+    }
+
+
+def test_delete_authorization_ok(session):
+    res = delete_authorization(authorization_id=3, connection=session)
+    assert res["statusCode"] == 201
+
+
+def test_delete_authorization_not_found(session):
+    res = delete_authorization(authorization_id=15, connection=session)
+    assert res["statusCode"] == 404
+
+
+def test_delete_handler_ok(session, populate_db, apigw_event):
+    app.CONNECTION = session
+    res = lambda_handler(apigw_event, None)
+    assert res["statusCode"] == 201
+
+
+def test_delete_handler_not_found(session, populate_db, apigw_event):
+    app.CONNECTION = session
+    apigw_event["pathParameters"]["id"] = 200
+    res = lambda_handler(apigw_event, None)
+    assert res["statusCode"] == 404
+
+
+def test_delete_handler_bad_input(session, populate_db, apigw_event):
+    app.CONNECTION = session
+    apigw_event["pathParameters"] = {"asd": 200}
+    res = lambda_handler(apigw_event, None)
+    assert res["statusCode"] == 400
