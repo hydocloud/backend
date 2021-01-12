aa3fa1a04702bd28eec04f5a90e67dbf29e43ac9
rtrive <riccardo.trivellato@gmail.com>
2021-01-12 19:51:56 +0100

    Device key

---
+++ stacks/hydo/secrets.py
@@ -0,0 +1,14 @@
+""" Stack to deploy https certificate """
+
+from aws_cdk import aws_secretsmanager as secret_manager
+
+
+def device_symmetric_key(self):
+    return secret_manager.Secret(
+        self,
+        "DeviceSecret",
+        secret_name="DeviceSecret",
+        generate_secret_string=secret_manager.SecretStringGenerator(
+            password_length=32
+        ),
+    )
