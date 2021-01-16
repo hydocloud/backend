2c1b9b0c5016be2b1b0d455d91fb8469bb970410
rtrive <riccardo.trivellato@gmail.com>
2021-01-16 21:31:35 +0100

    Get secret key from aws. Decrypt HMAC key

--- microservices/authorization/tests/test_validate_authorization.py
+++ microservices/authorization/tests/test_validate_authorization.py
@@ -1,11 +1,28 @@
 import pytest
+import boto3
 from datetime import datetime, timedelta
 from faker import Faker
+from moto import mock_secretsmanager
 from models.authorization import Unlock
-from models.devices import Devices, DevicesModel
+from models.devices import Devices
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from Crypto.Random import get_random_bytes
 
 
 faker = Faker()
+
+
+@mock_secretsmanager
+@pytest.fixture(scope="function")
+def secret():
+    with mock_secretsmanager():
+        conn = boto3.client("secretsmanager", region_name="eu-west-1")
+        conn.create_secret(
+            Name="java-util-test-password",
+            SecretString="ciaociaociaociaociaociaociaociao",
+        )
+        yield conn
 
 
 @pytest.fixture
@@ -34,11 +51,14 @@ def create_nonce(dynamodb):
 
 @pytest.fixture
 def create_device(session):
+    iv = get_random_bytes(AES.block_size)
+    cipher = AES.new(key=b"ciaociaociaociaociaociaociaociao", mode=AES.MODE_CBC, iv=iv)
+    data = b"ciaociao"
     device = Devices(
         name=faker.sentence(nb_words=1),
         serial=faker.sentence(nb_words=1),
         device_group_id=1,
-        hmac_key="ciaociaociaociaociaociaociaociao".encode(),
+        hmac_key=(iv + cipher.encrypt(pad(data, AES.block_size))),
         created_at=datetime.utcnow(),
         updated_at=datetime.utcnow(),
     )
@@ -79,9 +99,9 @@ def test_authorization_get_message(unlock, dynamodb, create_nonce, monkeypatch):
 
 
 class TestDeviceClass:
-
     def test_device_init(self):
         from validate_authorization.device import DeviceClass
+
         x = DeviceClass(100)
         assert x.device_id == 100
 
@@ -93,21 +113,32 @@ class TestDeviceClass:
 
         assert res.id == create_device.id
 
-    def test_get_hmac(self, session, create_device):
+    def test_get_hmac(self, session, create_device, monkeypatch):
         from validate_authorization.device import DeviceClass
 
-        x = DeviceClass(create_device.id)
-        res = x.get_hmac(connection=session)
+        monkeypatch.setenv("SECRET_NAME", "DeviceSecret")
 
-        assert res.id == create_device.id
-        assert res.hmac_key == create_device.hmac_key
-        assert x.hmac_key == create_device.hmac_key
+        x = DeviceClass(create_device.id)
+        x.get_hmac(connection=session, key=b"ciaociaociaociaociaociaociaociao")
+
+        assert x.hmac_key == "ciaociao".encode("utf-8")
 
     def test_digest(self, create_device, session):
         from validate_authorization.device import DeviceClass
 
         x = DeviceClass(create_device.id)
-        x.hmac_key = create_device.hmac_key
+        x.hmac_key = b"ciaociaociaociaociaociaociaociao"
         res = x.digest(message="ciao")
 
         assert res == "d5cfd1d870147b8f1cbced82e601233e74b671710fcf83aef82d2cbd0e7f4675"
+
+    def test_crypt_get_secret_ok(self, create_device, secret, monkeypatch):
+        from validate_authorization.device import DeviceClass
+        monkeypatch.setenv("SECRET_NAME", "java-util-test-password")
+
+        x = DeviceClass(create_device.id)
+        res = x.get_secret_key()
+
+        assert type(res) == bytes
+        assert res.decode() == "ciaociaociaociaociaociaociaociao"
+    
--- microservices/authorization/validate_authorization/device.py
+++ microservices/authorization/validate_authorization/device.py
@@ -1,6 +1,11 @@
 import logging
+import boto3
 from Crypto.Hash import HMAC, SHA256
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
 from typing import Optional
+from os import environ
+from botocore.exceptions import ClientError
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm.session import Session
 from models.devices import Devices
@@ -21,19 +26,35 @@ class DeviceClass:
             logger.error(err)
         return None
 
-    def get_hmac(self, connection: Session) -> Optional[Devices]:
+    def get_hmac(self, key: bytes, connection: Session) -> Optional[Devices]:
         try:
             res = connection.query(Devices.id, Devices.hmac_key).filter_by(
                 id=self.device_id
             ).first()
-            self.hmac_key = res.hmac_key
-            return res
+            iv = res.hmac_key[:AES.block_size]
+            cipher = AES.new(key=key, mode=AES.MODE_CBC, iv=iv)
+            self.hmac_key = unpad(cipher.decrypt(res.hmac_key[AES.block_size:]), AES.block_size)
+
         except SQLAlchemyError as err:
             logger.error(err)
         return None
 
     def digest(self, message: str) -> str:
-
         h = HMAC.new(self.hmac_key, digestmod=SHA256)
         h.update(message.encode())
         return h.hexdigest()
+
+    def get_secret_key(self, secret_manager=None) -> bytes:
+        secret_name = environ["SECRET_NAME"]
+        if secret_manager is None:
+            session = boto3.session.Session()
+            secret_manager = session.client(service_name="secretsmanager")
+
+        try:
+            get_secret_value_response = secret_manager.get_secret_value(
+                SecretId=secret_name
+            )
+            return get_secret_value_response["SecretString"].encode()
+        except ClientError as err:
+            logger.error(err)
+            raise err
