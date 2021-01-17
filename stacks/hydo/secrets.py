181690031f1121a0c99a202c9623e55b430d5e34
rtrive <riccardo.trivellato@gmail.com>
2021-01-17 11:59:44 +0100

    Lint and add return type

--- stacks/hydo/secrets.py
+++ stacks/hydo/secrets.py
@@ -3,12 +3,10 @@
 from aws_cdk import aws_secretsmanager as secret_manager
 
 
-def device_symmetric_key(self):
+def device_symmetric_key(self) -> secret_manager.Secret:
     return secret_manager.Secret(
         self,
         "DeviceSecret",
         secret_name="DeviceSecret",
-        generate_secret_string=secret_manager.SecretStringGenerator(
-            password_length=32
-        ),
+        generate_secret_string=secret_manager.SecretStringGenerator(password_length=32),
     )
