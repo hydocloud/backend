3c34543a92a6a0be7eb523fd90321f0280940b08
rtrive <riccardo.trivellato@gmail.com>
2021-01-17 14:42:07 +0100

    Update cdk

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     author="author",
     package_dir={"": "stacks"},
     packages=setuptools.find_packages(where="stacks"),
-    install_requires=["aws-cdk.core==1.74.0",],
+    install_requires=["aws-cdk.core==1.80.0",],
     python_requires=">=3.6",
     classifiers=[
         "Development Status :: 4 - Beta",
