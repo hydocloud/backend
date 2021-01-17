123b05a3ee365c4c2627adfbac2a5f52156f85d7
rtrive <riccardo.trivellato@gmail.com>
2021-01-17 14:42:53 +0100

    Remove useless code

--- microservices/authorization/authorization_service/app.py
+++ microservices/authorization/authorization_service/app.py
@@ -1,32 +1,16 @@
 import json
 import logging
-from models.wallet import Wallet  # type: ignore
 from store_nonce import store as store_nonce  # type: ignore
 import asyncio
-import urllib3  # type: ignore
 
 loop = asyncio.get_event_loop()
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG)
-wallet_handle = None
-did = None
-verkey = None
-http = urllib3.PoolManager()
-
-
-async def init_wallet():
-    global wallet_handle
-
-    if wallet_handle is None:
-        x = Wallet()
-        wallet_handle = await x.open_wallet()
 
 
 def lambda_handler(event, context):
-    logger.info("Open wallet")
-    loop.run_until_complete(init_wallet())
     try:
         payload = event["body"]
         encrypted_message = json.loads(payload)["message"]
