c5fa9aadecd372d50e14f01b4db11d8d5c0564c4
rtrive <riccardo.trivellato@gmail.com>
2021-04-13 17:16:49 +0200

    Add fake org creation

--- microservices/organization/conftest.py
+++ microservices/organization/conftest.py
@@ -1,7 +1,9 @@
 import pytest
+import uuid
+import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models.organizations import Base
+from models.organizations import Base, Organization
 
 
 @pytest.fixture(scope="function")
@@ -15,3 +17,19 @@ def setup_database():
     session = Session()
     yield session
     session.close()
+
+
+@pytest.fixture(scope="function")
+def setup_org_id(setup_database):
+
+    org = Organization( 
+        name = "asd",
+        license_id = 1,
+        owner_id=uuid.uuid4().__str__(),
+        created_at=datetime.datetime.utcnow(),
+        updated_at=datetime.datetime.utcnow(),
+    )
+    setup_database.add(org)
+    setup_database.commit()
+    setup_database.refresh(org)
+    return org
