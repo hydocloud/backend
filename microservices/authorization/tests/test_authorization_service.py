33301b89fef2b81e78dfd5db049e1773acf0ff7f
rtrive <riccardo.trivellato@gmail.com>
2021-04-26 20:11:21 +0200

    Restore test

--- microservices/authorization/tests/test_authorization_service.py
+++ microservices/authorization/tests/test_authorization_service.py
@@ -25,11 +25,11 @@ def test_store_nonce_ko(dynamodb, monkeypatch):
     assert res is False
 
 
-# def test_store_nonce_ko_with_no_dynamodb(dynamodb, monkeypatch):
-#     monkeypatch.setenv("SERVICE_ID", "7bacf67c-4c3b-4e30-83fb-a96a93fd2c74")
-#     monkeypatch.setenv("NONCE_TABLE_NAME", "authorization_sessions")
-#     monkeypatch.setenv("DYNAMODB_ENDPOINT_OVERRIDE", "sadsadasd")
-#     message = "test"
+def test_store_nonce_ko_with_no_dynamodb(dynamodb, monkeypatch):
+    monkeypatch.setenv("SERVICE_ID", "7bacf67c-4c3b-4e30-83fb-a96a93fd2c74")
+    monkeypatch.setenv("NONCE_TABLE_NAME", "authorization_sessions")
+    monkeypatch.setenv("DYNAMODB_ENDPOINT_OVERRIDE", "sadsadasd")
+    message = "test"
 
-#     res = store(message, None)
-#     assert res is False
+    res = store(message, None)
+    assert res is False
