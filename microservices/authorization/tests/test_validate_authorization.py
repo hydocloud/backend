06871b20d383ab59aca29714dec805d85ff5cd1b
rtrive <riccardo.trivellato@gmail.com>
2021-04-26 19:56:51 +0200

    Comment local tests

--- microservices/authorization/tests/test_validate_authorization.py
+++ microservices/authorization/tests/test_validate_authorization.py
@@ -121,21 +121,21 @@ def test_authorization_get_message(
     assert x.service_message == "test"
 
 
-def test_authorization_get_message_local(
-    unlock, dynamodb, create_nonce, monkeypatch, authorizations_session
-):
-    from src.validate_authorization.authorization import AuthorizationClass
+# def test_authorization_get_message_local(
+#     unlock, dynamodb, create_nonce, monkeypatch, authorizations_session
+# ):
+#     from src.validate_authorization.authorization import AuthorizationClass
 
-    monkeypatch.setenv("SERVICE_ID", "7bacf67c-4c3b-4e30-83fb-a96a93fd2c74")
-    monkeypatch.setenv("NONCE_TABLE_NAME", "authorization_sessions")
-    monkeypatch.setenv(
-        "DYNAMODB_ENDPOINT_OVERRIDE", "http://MacBook-Pro-di-Riccardo.local:8000"
-    )
+#     monkeypatch.setenv("SERVICE_ID", "7bacf67c-4c3b-4e30-83fb-a96a93fd2c74")
+#     monkeypatch.setenv("NONCE_TABLE_NAME", "authorization_sessions")
+#     monkeypatch.setenv(
+#         "DYNAMODB_ENDPOINT_OVERRIDE", "http://MacBook-Pro-di-Riccardo.local:8000"
+#     )
 
-    x = AuthorizationClass(obj=unlock, connection=authorizations_session)
-    x.get_message(dynamodb=dynamodb)
+#     x = AuthorizationClass(obj=unlock, connection=authorizations_session)
+#     x.get_message(dynamodb=dynamodb)
 
-    assert x.service_message == "test"
+#     assert x.service_message == "test"
 
 
 class TestDeviceClass:
